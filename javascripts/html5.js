699b5bd25ffbc59e43eb61dbd5233b7624bcd581
Owen Pletz <voc.opletz@yahoo.com>
2012-03-07 11:33:50 -0800

    Initial commit

---
+++ javascripts/html5.js
@@ -0,0 +1,8 @@
+//Use Javascript to create HTML5 elements for IE7,IE6
+//NOTE: Elements must be created as the document loads.
+//Creating elements after document load (window.onload) is too late.
+
+  document.createElement('header');
+  document.createElement('nav');
+  document.createElement('aside');
+  document.createElement('footer');
